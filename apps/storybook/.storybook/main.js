0b02a5411d323679ade5e836776cf196d051bb65
Jhonathan <nathan3boss@gmail.com>
2023-04-01 14:47:07 -0300

    feat(storybook): remove scripts

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,7 +1,12 @@
 module.exports = {
-  stories: ['../../../libs/core/src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  typescript: {
-    check: true,
-  },
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
 };
--- apps/storybook/.storybook/preview.js
+++ apps/storybook/.storybook/preview.js
@@ -1,4 +1,24 @@
-const { addDecorator } = require('@storybook/react');
-const { withKnobs } = require('@storybook/addon-knobs');
+import React from 'react';
 
-addDecorator(withKnobs);
+import { PharThemeProvider } from '@phar/core';
+import themes from '@phar/themes';
+
+import './styles/global.css';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  Story => (
+    <PharThemeProvider theme={themes.dark}>
+      <Story />
+    </PharThemeProvider>
+  ),
+];
